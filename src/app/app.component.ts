fafdc5472e33ba681f7af237cda2a0a46b56a13d
alone <alonecapponi@gmail.com>
2026-09-21 19:33:03 +0200

    lezione 4 2.30

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cibando';
+
+images = [
+  {id: 1, label: 'Spaghetti al sugo'},
+  {id: 2, label: 'tagliata di manzo'},
+  {id: 3, label: 'Tiramisu'},
+]
+
+  percorsoFoto = "../assets/images/imageBg-1.jpg";
+
+
+allievi = [
+  {nome: 'alone', citta: 'roma'},
+  {nome: 'itria', citta: 'roma'},
+  {nome: 'mary', citta: 'napoli'},
+  {nome: 'alone', citta: 'milano'},
+];
+
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -13,6 +13,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
       AppRoutingModule,
       BrowserAnimationsModule,
+      NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
