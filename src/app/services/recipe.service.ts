50a5170d940031c788c6ef92c432e15b1ddf9ee6
alone <alonecapponi@gmail.com>
2026-09-24 18:24:43 +0200

    sesta lezione creazione modello, mock,service e...

---
+++ src/app/services/recipe.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { Recipe } from '../models/recipe.model';
+import { RECIPES } from '../mocks/recipe.mock';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipeService {
+
+  constructor() { }
+}
